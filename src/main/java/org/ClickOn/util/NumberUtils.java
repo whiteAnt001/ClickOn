642cd5750b74ce3ea8767ef870a4c043a8fb1e64
박준서 <westjun01@gmail.com>
2025-09-30 16:42:18 +0900

    Update NumberUtils.java

--- src/main/java/org/ClickOn/util/NumberUtils.java
+++ src/main/java/org/ClickOn/util/NumberUtils.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 import java.text.DecimalFormat;
 
+// 가격표 포멧팅
 @Component("numberUtils")
 public class NumberUtils {
     public String formatWithComma(Number number) {
